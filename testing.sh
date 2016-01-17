8199b3a186f0b64d5aee9ab4fd16dc54b5b923d7
wwong38519 <wwong38519@gmail.com>
2016-01-17 23:59:46 +0800

    add comment upvote

--- testing.sh
+++ testing.sh
@@ -12,3 +12,12 @@ curl http://localhost:3000/posts/569ba45d90614ecb4cf50b83
 curl -X PUT http://localhost:3000/posts/569ba45d90614ecb4cf50b83/upvote
 #{"_id":"569ba45d90614ecb4cf50b83","title":"test","link":"http://www.google.com","__v":0,"comments":[],"upvotes":1}
 
+curl --data "body=comment&author=somebody" http://localhost:3000/posts/569ba45d90614ecb4cf50b83/comments
+#{"__v":0,"post":{"_id":"569ba45d90614ecb4cf50b83","title":"test","link":"http://www.google.com","__v":1,"comments":["569bb9241a65662e4d623e69"],"upvotes":1},"body":"comment","author":"somebody","_id":"569bb9241a65662e4d623e69","upvotes":0}
+
+curl -X PUT http://localhost:3000/posts/569ba45d90614ecb4cf50b83/comments/569bb9241a65662e4d623e69/upvote
+#{"_id":"569bb9241a65662e4d623e69","post":"569ba45d90614ecb4cf50b83","body":"comment","author":"somebody","__v":0,"upvotes":2}
+
+curl http://localhost:3000/posts
+#[{"_id":"569ba45d90614ecb4cf50b83","title":"test","link":"http://www.google.com","__v":1,"comments":["569bb9241a65662e4d623e69"],"upvotes":1}]
+
