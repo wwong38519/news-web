83c855a58288d49535dbc405f43caa46b9301953
wwong38519 <wwong38519@gmail.com>
2016-01-17 22:45:29 +0800

    add param function to load objects before handling by route

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// GET posts - mongoose function: find
 router.get('/posts', function(req, res, next) {
 	Post.find(function(err, posts) {
 		if (err) return next(err);
@@ -17,12 +18,29 @@ router.get('/posts', function(req, res, next) {
 	});
 });
 
+// POST posts - mongoose function: save
 router.post('/posts', function(req, res, next) {
 	var post = new Post(req.body);
 	post.save(function(err, post) {
 		if (err) return next(err);
 		res.json(post);
 	});
+});
+
+// run before route if route url contains :post
+// Express function : param
+router.param('post', function(req, res, next, id) {
+	var query = Post.findById(id);
+	query.exec(function(err, post) {
+		if (err) return next(err);
+		if (!post) return next(new Error('Post not found'));
+		req.post = post;
+		return next();
+	});
+});
+
+router.get('/posts/:post', function(req, res) {
+	res.json(req.post);
 });
 
 module.exports = router;
