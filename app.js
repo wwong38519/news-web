d86472da5ca89c62ed2cbe29967d12c2d3e4a7ae
wwong38519 <wwong38519@gmail.com>
2016-01-17 21:53:17 +0800

    fix typo

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongooes = require('mongoose');
+var mongoose = require('mongoose');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
